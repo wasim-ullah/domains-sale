1a42426f360b1c27645aac206795dc3ccaac7d3b
Wasim Ullah <wasim.ullah@aol.com>
2023-06-26 17:06:48 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,6 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        A Wasim.co Entity
       </footer>
     </>
   )
